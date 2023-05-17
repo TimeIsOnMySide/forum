67ca09b8a2061094466ad72951f7bf328af39ba9
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-05-17 15:01:47 -0700

    Working but have some console violations..

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// your code here
+
+// Listen for a click on the 'post' button
+document.getElementById('submit').addEventListener('click', function() {
+  // Create a string variable to store the text from both text firelds.
+  let name = document.getElementById('name').value;
+  let message = document.getElementById('message').value;
+
+  //add info to the .posts div
+  document.querySelector('.posts').innerHTML = `${message} <br> Posted By: ${name}`;
+});
